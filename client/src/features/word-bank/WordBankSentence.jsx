1af61b1dcc08aea3462f190850ee2850fd4de768
Michelle Tanzil <mtanzil01@gmail.com>
2020-08-22 13:14:42 -0700

    Sentence component

---
+++ client/src/features/word-bank/WordBankSentence.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { observer } from "mobx-react-lite";
+import { useContext } from "react";
+import { RootStoreContext } from "../../app/stores/rootStore";
+import { Container } from "semantic-ui-react";
+import TextInput from "../../app/common/form/TextInput";
+
+const WordBankSentence = () => {
+  const rootStore = useContext(RootStoreContext);
+  const { sentence, clearSentence, textToSpeech } = rootStore.SentenceStore;
+
+  return (
+    <Container text>
+      <p>{sentence}</p>
+      <Button
+        floated="left"
+        animated
+        color="red"
+        inverted
+        onClick={clearSentence}
+      >
+        <Button.Content visible>Clear</Button.Content>
+        <Button.Content hidden>
+          <Icon name="trash" />
+        </Button.Content>
+      </Button>
+      <Button
+        floated="right"
+        animated
+        color="blue"
+        inverted
+        onClick={textToSpeech}
+      >
+        <Button.Content visible>Speak</Button.Content>
+        <Button.Content hidden>
+          <Icon name="volume up" />
+        </Button.Content>
+      </Button>
+    </Container>
+  );
+};
+
+export default observer(WordBankSentence);
