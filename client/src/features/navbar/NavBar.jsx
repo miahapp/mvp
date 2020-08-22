55fbacc4c98e765b903a7295566f8e00ddb02ab6
Michelle Tanzil <mtanzil01@gmail.com>
2020-08-22 10:15:59 -0700

    Fixed navbar

--- client/src/features/navbar/NavBar.jsx
+++ client/src/features/navbar/NavBar.jsx
@@ -27,29 +27,40 @@ const NavBar = () => {
         >
           Home
         </Menu.Item>
-        <Menu.Item
-          as={NavLink}
-          to="/wordcount"
-          active={activeItem === "wordcount"}
-          name="wordcount"
-          onClick={handleItemClick}
-        >
-          Stats
-        </Menu.Item>
         {user ? (
-          <Menu.Item position="right">
-            <Dropdown pointing="top left" text={user.displayName}>
-              <Dropdown.Menu>
-                <Dropdown.Item
-                  as={Link}
-                  to={`/profile/${user.username}`}
-                  text="My profile"
-                  icon="user"
-                />
-                <Dropdown.Item onClick={logout} text="Logout" icon="power" />
-              </Dropdown.Menu>
-            </Dropdown>
-          </Menu.Item>
+          <Menu.Menu>
+            <Menu.Item
+              as={NavLink}
+              to="/wordcount"
+              active={activeItem === "wordcount"}
+              name="wordcount"
+              onClick={handleItemClick}
+            >
+              Stats
+            </Menu.Item>
+            <Menu.Item
+              as={NavLink}
+              to="/wordbank"
+              active={activeItem === "wordbank"}
+              name="wordbank"
+              onClick={handleItemClick}
+            >
+              Bank
+            </Menu.Item>
+            <Menu.Item position="right">
+              <Dropdown pointing="top left" text={user.displayName}>
+                <Dropdown.Menu>
+                  <Dropdown.Item
+                    as={Link}
+                    to={`/profile/${user.username}`}
+                    text="My profile"
+                    icon="user"
+                  />
+                  <Dropdown.Item onClick={logout} text="Logout" icon="power" />
+                </Dropdown.Menu>
+              </Dropdown>
+            </Menu.Item>
+          </Menu.Menu>
         ) : (
           <Menu.Item position="right">
             <Button as={NavLink} to="/login" inverted color="teal">
