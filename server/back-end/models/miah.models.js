0f93869f47ff8fc727639c9c7d7c52ebdc6fefea
daesung626 <57124915+daesung626@users.noreply.github.com>
2020-08-27 17:13:49 -0700

    Fixing conflicts

--- server/back-end/models/miah.models.js
+++ server/back-end/models/miah.models.js
@@ -11,4 +11,14 @@ const User = function (user) {
     this.password = user.password;
 };
 
+const Word = function (word) {
+    this.word_name = word.word_name;
+    this.created_at = Date.now()// add new timestamp ;
+    this.updated_at = Date.now()// add new timestamp ;
+    this.use_yn = word.use_yn;
+    this.word_icon = word.word_icon;
+    this.category_idx = word.category_idx;
+}
+
 module.exports = User;
+module.exports = Word;
--- server/back-end/routes/miah.routes.js
+++ server/back-end/routes/miah.routes.js
@@ -5,6 +5,12 @@ module.exports = app => {
     app.post('/api/register', miahController.register);
     app.post('/api/login', miahController.login);
     app.post('/api/click', miahController.click);
+    app.post('/api/addWord', wordController.addWord);
+    app.get('/api/allWord', wordController.allWord);
+    app.get('/api/findById', wordController.findById);
+    app.get('/api/findByName', wordController.findByName);
+    app.post('/api/delete', wordController.delete);
+    app.post('/api/update', wordController.update);
 };
 
 
