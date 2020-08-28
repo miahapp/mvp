713da767b54ca78478c6494c93ea248077771e4a
Michelle Tanzil <mtanzil01@gmail.com>
2020-08-28 11:16:21 -0700

    Amazon Polly integration

--- client/src/app/stores/sentenceStore.js
+++ client/src/app/stores/sentenceStore.js
@@ -1,6 +1,42 @@
 import { decorate, observable, action, runInAction } from "mobx";
 import agent from "../api/agent";
 
+const AWS = require("aws-sdk");
+
+// Create an Polly client
+const Polly = new AWS.Polly({
+    signatureVersion: 'v4',
+    region: 'us-west-1'
+})
+
+// Create the Speaker instance
+const Player = new Speaker({
+  channels: 1,
+  bitDepth: 16,
+  sampleRate: 16000
+})
+
+let params = {
+    'Text': this.sentence,
+    'OutputFormat': 'mp3',
+    'VoiceId': 'Kimberly'
+}
+
+const SPEAKER = Polly.synthesizeSpeech(params, (err, data) => {
+    if (err) {
+        console.log(err.code)
+    } else if (data) {
+        if (data.AudioStream instanceof Buffer) {
+            // Initiate the source
+            var bufferStream = new Stream.PassThrough()
+            // convert AudioStream into a readable stream
+            bufferStream.end(data.AudioStream)
+            // Pipe into Player
+            bufferStream.pipe(Player)
+        }
+    }
+})
+
 export default class SentenceStore {
   constructor(rootStore) {
     this.rootStore = rootStore;
@@ -27,7 +63,13 @@ export default class SentenceStore {
   };
 
   // Speech to text api integration
-  textToSpeech = () => {};
+  textToSpeech = () => {
+    try {
+      this.SPEAKER();
+    } catch (error) {
+      toast.error("Problem with the TTS");
+    }
+  };
 }
 decorate(SentenceStore, {
   sentence: observable,
