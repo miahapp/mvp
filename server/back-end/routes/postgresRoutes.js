ee12d51ec9960bd16923819563a78634d6f9a048
nalin97 <nalin97@github.com>
2020-09-05 18:18:12 -0700

    postgres added

---
+++ server/back-end/routes/postgresRoutes.js
@@ -0,0 +1,13 @@
+const userdb = require("../controllers/postgresUser.js");
+const worddb = require("../controllers/postgresWord.js");
+
+module.exports = app => {
+    app.post('/users', userdb.addUser)
+    app.get('/users',userdb.getUsers)
+    app.delete('/users/:id',userdb.deleteUser)
+    app.post('/users/login',userdb.login)
+    app.get('/words',worddb.getWords)
+    app.get('/words/:id',worddb.wordById)
+    app.post('/words',worddb.addWord)
+    app.delete('/words/:id',worddb.deleteWord)
+};
