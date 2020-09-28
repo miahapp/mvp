11cf3ddfad0d6bd064dcf34679da194979b4d969
Nathanael Wa Mwenze <11463376+nmwenz90@users.noreply.github.com>
2020-09-28 15:59:33 -0700

    modified server js

--- server/server.js
+++ server/server.js
@@ -9,11 +9,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 require('dotenv').config();
 //restricting the request to just localhost3000
-app.use(
-  cors({
-    origin: 'http://localhost:3000',
-  })
-);
+app.use(cors());
 
 const session = require('express-session');
 app.use(
@@ -44,23 +40,24 @@ connection.connect(function (err) {
 });
 module.exports = connection;
 
-// Simple route
-app.get('/', (req, res) => {
-  if (!req.session.userID) {
-    console.log('Welcome to miah');
-    res.json({ message: 'Welcome to miah' });
-  } else {
-    res.json({ message: 'NOT LOGGED IN' });
-    console.log('NOT LOGGED IN');
-  }
-});
-
-require('./back-end/routes/miah.routes')(app);
-
 //for heroku (production mode)
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  app.use(express.static('/../client/build'));
+} else {
+  app.use(express.static('/../client/build'));
 }
+// Simple route
+// app.get('/', (req, res) => {
+//   if (!req.session.userID) {
+//     console.log('Welcome to miah');
+//     res.json({ message: 'Welcome to miah' });
+//   } else {
+//     res.json({ message: 'NOT LOGGED IN' });
+//     console.log('NOT LOGGED IN');
+//   }
+// });
+
+require('./back-end/routes/miah.routes')(app);
 
 // Port
 app.listen(PORT, () => console.log(`listening on port ${process.env.PORT}`));
