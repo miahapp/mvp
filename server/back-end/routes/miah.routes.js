3ea493da61685352c00a0c9f2c7cb274e0f2f69a
Nathanael Wa Mwenze <11463376+nmwenz90@users.noreply.github.com>
2020-09-29 22:09:48 -0700

    updated wordcontroller import in miahroutes js

--- server/back-end/routes/miah.routes.js
+++ server/back-end/routes/miah.routes.js
@@ -1,14 +1,14 @@
-const miahController = require("../controllers/miah.controllers.js");
-const wordController = require("../controllers/WordControllers.js");
+const miahController = require('../controllers/miah.controllers.js');
+const wordController = require('../controllers/wordControllers.js');
 module.exports = (app) => {
-  app.post("/api/register", miahController.register);
-  app.post("/api/login", miahController.login);
-  app.post("/api/click", miahController.click);
-  app.post("/api/addWord", wordController.addWord);
-  app.get("/api/allWord", wordController.allWord);
-  app.get("/api/findById", wordController.findById);
-  app.get("/api/findByName", wordController.findByName);
-  app.post("/api/delete", wordController.delete);
-  app.post("/api/update", wordController.update);
-  app.get("/api/categories", wordController.categories);
+  app.post('/api/register', miahController.register);
+  app.post('/api/login', miahController.login);
+  app.post('/api/click', miahController.click);
+  app.post('/api/addWord', wordController.addWord);
+  app.get('/api/allWord', wordController.allWord);
+  app.get('/api/findById', wordController.findById);
+  app.get('/api/findByName', wordController.findByName);
+  app.post('/api/delete', wordController.delete);
+  app.post('/api/update', wordController.update);
+  app.get('/api/categories', wordController.categories);
 };
